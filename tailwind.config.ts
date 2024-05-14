1a1dc1ed06325483ba33c90aaaa5c2a136efc24c
SMTanimur <smtanimurrahman404@gmail.com>
2024-05-14 11:40:32 +0600

    Update styles and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,106 +1,80 @@
-import type { Config } from "tailwindcss";
-const plugin = require('tailwindcss/plugin');
-const rotateX = plugin(function ({ addUtilities }:any) {
-    addUtilities({
-        '.rotate-y-180': {
-            transform: 'rotateY(180deg)',
-        },
-    });
-});
-const config: Config = {
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-            DEFAULT: '#d946ef',
-            light: '#eaf1ff',
-            'dark-light': 'rgba(67,97,238,.15)',
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-            DEFAULT: '#805dca',
-            light: '#ebe4f7',
-            'dark-light': 'rgb(128 93 202 / 15%)',
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        success: {
-            DEFAULT: '#00ab55',
-            light: '#ddf5f0',
-            'dark-light': 'rgba(0,171,85,.15)',
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        danger: {
-            DEFAULT: '#e7515a',
-            light: '#fff5f5',
-            'dark-light': 'rgba(231,81,90,.15)',
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        warning: {
-            DEFAULT: '#e2a03f',
-            light: '#fff9ed',
-            'dark-light': 'rgba(226,160,63,.15)',
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        info: {
-            DEFAULT: '#2196f3',
-            light: '#e7f7ff',
-            'dark-light': 'rgba(33,150,243,.15)',
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        dark: {
-            DEFAULT: '#3b3f5c',
-            light: '#eaeaec',
-            'dark-light': 'rgba(59,63,92,.15)',
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        black: {
-            DEFAULT: '#0e1726',
-            light: '#e3e4eb',
-            'dark-light': 'rgba(14,23,38,.15)',
-        },
-        white: {
-            DEFAULT: '#ffffff',
-            light: '#e0e6ed',
-            dark: '#888ea8',
-        },
-    },
-    fontFamily: {
-      nunito: ['var(--font-nunito)'],
-  },
-  spacing: {
-      4.5: '18px',
-  },
-  boxShadow: {
-      '3xl': '0 2px 2px rgb(224 230 237 / 46%), 1px 6px 7px rgb(224 230 237 / 46%)',
-  },
-  typography: ({ theme }:any) => ({
-      DEFAULT: {
-          css: {
-              '--tw-prose-invert-headings': theme('colors.white.dark'),
-              '--tw-prose-invert-links': theme('colors.white.dark'),
-              h1: { fontSize: '40px', marginBottom: '0.5rem', marginTop: 0 },
-              h2: { fontSize: '32px', marginBottom: '0.5rem', marginTop: 0 },
-              h3: { fontSize: '28px', marginBottom: '0.5rem', marginTop: 0 },
-              h4: { fontSize: '24px', marginBottom: '0.5rem', marginTop: 0 },
-              h5: { fontSize: '20px', marginBottom: '0.5rem', marginTop: 0 },
-              h6: { fontSize: '16px', marginBottom: '0.5rem', marginTop: 0 },
-              p: { marginBottom: '0.5rem' },
-              li: { margin: 0 },
-              img: { margin: 0 },
-          },
       },
-  }),
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms')({
-        strategy: 'class',
-    }),
-    require('@tailwindcss/typography'),
-    rotateX,
-],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
