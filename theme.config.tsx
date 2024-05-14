1b27c38db1896636071d6adfaced20ab74a0866a
SMTanimur <smtanimurrahman404@gmail.com>
2024-05-14 14:21:02 +0600

    Refactor code to improve performance and readability

---
+++ theme.config.tsx
@@ -0,0 +1,8 @@
+const themeConfig = {
+    locale: 'en',
+
+    animation: '', // animate__fadeIn, animate__fadeInDown, animate__fadeInUp, // navbar-sticky, navbar-floating, navbar-static
+    semidark: false,
+};
+
+export default themeConfig;
