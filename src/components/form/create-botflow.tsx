c65bb7dc41f08b03e7388723f49e783a2ef82af5
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-08-30 11:34:14 +0600

    refactor: Update botflow node handling and dependencies

--- src/components/form/create-botflow.tsx
+++ src/components/form/create-botflow.tsx
@@ -1,79 +1,31 @@
 "use client"
 import React from 'react'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
-import { useWorkflow } from '@/hooks'
-import { useGlobalModalStateStore } from '@/store/modal'
 import { Input } from '../ui/input'
-import { nanoid } from "nanoid";
 import { Textarea } from '../ui/textarea'
 import { Button } from '../ui/button'
 import { Icons } from '../ui/icons'
-import { CreateWorkflowDto } from '@/types/workflow'
-import { toast } from '@/lib'
-import { IBotFlows, useBotFlowsStore } from '@/store/botfllow/botflows'
-import { Node } from '@xyflow/react'
-import { IBotNodeData } from '@/types'
+
+
+import { useBotFlow } from '@/hooks/botflow/useBotflow'
 
 const CreateBotflowForm = () => {
 
-  const {workflowForm,isCreating}=useWorkflow()
-  const {addBotflows}=useBotFlowsStore()
-  const {setBotflowModal}=useGlobalModalStateStore()
 
-  
-  const createBotflow = async (data:CreateWorkflowDto) => {
-     try {
-       const id = nanoid()
 
-      const initialNode:IBotNodeData={
-        id:"start_node",
-        data:{
-          icon:"dress",
-          label:"Starting Point",
-          description:"where you bot begins",
-          botflowId:id,
-        },
-        position: { x: 0, y: 0 },
-        description: "", // Add the missing property
-        type: "start",
-        dragging: false,
-        zIndex: 0,
-        isConnectable: true,
-        positionAbsoluteX: 0, // Add the missing property
-        positionAbsoluteY: 0, // Add the missing property
-     
-      }
-       const botData:IBotFlows= {
-        id,
-        description: data?.description as string,
-        name: data.name,
-        nodes:[initialNode],
-        edges:[]
-       }
-       addBotflows(botData)
-       toast({
-        title: 'Botflow created successfully',
-        icon:'success',
-      });
-     setBotflowModal(false)
-     } catch (error:any) {
-      toast({
-        title: error,
-        icon: 'error',
-      });
-     }
-    }
+
+    const {botflowForm,createBotflow,isCreating}=useBotFlow()
   return (
     <div className='pb-4'>
-    <Form {...workflowForm} >
+    <Form {...botflowForm} >
     <form
     className='grid gap-6'
     onSubmit={(...args) =>
-      void workflowForm.handleSubmit(createBotflow)(...args)
+      void botflowForm.handleSubmit(createBotflow)(...args)
     }
   >
     <FormField
-      control={workflowForm.control}
+      control={botflowForm.control}
       name='name'
       render={({ field }) => (
         <FormItem>
@@ -88,7 +40,7 @@ const CreateBotflowForm = () => {
 
     
     <FormField
-      control={workflowForm.control}
+      control={botflowForm.control}
       name='description'
       render={({ field }) => (
         <FormItem>
