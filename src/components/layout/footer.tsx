ee525ceccafaa94d2740c9fcdab271069faa8e2a
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-10-08 18:59:14 +0600

    Refactor footer component: Remove unused code

--- src/components/layout/footer.tsx
+++
@@ -1,5 +0,0 @@
-export const Footer = () => {
-    return <div className="mt-auto p-6 pt-0 text-center dark:text-white-dark sm:text-left  ">© {new Date().getFullYear()}. SMTR All rights reserved.</div>;
-};
-
-
