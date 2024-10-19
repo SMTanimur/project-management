a0304fa520ad60b5dd692e6cfbc40c6771379f31
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-10-19 10:21:01 +0600

    Refactor getSession function to remove unused code

--- src/utils/getSessoin.ts
+++ src/utils/getSessoin.ts
@@ -1,5 +1,4 @@
 export function getSession(): boolean {
-  // Check if the session cookie exists
-  // This example assumes the session cookie is named 'orga_sid'
+
   return document.cookie.split('; ').some((cookie) => cookie.startsWith('orga_sid='));
 }
