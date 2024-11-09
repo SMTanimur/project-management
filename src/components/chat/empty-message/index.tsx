e795d9b8c7954c0fab2c9e141b1180a309f44a17
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-11-09 09:29:34 +0600

    style: Refactor blank and empty message components for consistency and readability

--- src/components/chat/empty-message/index.tsx
+++ src/components/chat/empty-message/index.tsx
@@ -1,17 +1,20 @@
-"use client"
+'use client';
 import { Icon } from '@iconify/react';
 export const EmptyMessage = () => {
   return (
-    <div className="h-full flex flex-col justify-center">
-      <div className="h-full flex justify-center items-center">
-        <div className="text-center flex flex-col items-center">
-          <Icon icon="typcn:messages" className="text-7xl text-default-300" />
-          <div className="mt-4 text-lg font-medium text-default-500">No messages </div>
-          <div className="mt-1 text-sm font-medium text-default-400">don't worry, just take a deep breath & say "Hello"</div>
+    <div className='h-full flex flex-col justify-center'>
+      <div className='h-full flex justify-center items-center'>
+        <div className='text-center flex flex-col items-center'>
+          <Icon icon='typcn:messages' className='text-7xl text-default-300' />
+          <div className='mt-4 text-lg font-medium text-default-500'>
+            No messages{' '}
+          </div>
+          <div className='mt-1 text-sm font-medium text-default-400'>
+            don&apos;t worry, just take a deep breath &amp; say
+            &quot;Hello&quot;
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
-
-
