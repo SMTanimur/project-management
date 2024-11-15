77f68115a863124b8507013a56ed78f27f1f9ec3
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-11-15 17:50:14 +0600

    fix: Enhance session middleware logic to handle redirects for login page more effectively

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,21 +5,21 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
   const sessionToken = req.cookies.get('orga_sid')?.value;
 
-  if (!sessionToken) {
-    // If the session token is not present, redirect to the login page
+  // If the session token is not present and the user is not on the login page, redirect to the login page
+  if (!sessionToken && req.nextUrl.pathname !== '/auth/login') {
     return NextResponse.redirect(new URL('/auth/login', req.url));
   }
- 
 
-  if(sessionToken && req.nextUrl.pathname === '/auth/login') {
-    return NextResponse.redirect(new URL('/', req.url))
+  // If the session token is present and the user is on the login page, redirect to the home page
+  if (sessionToken && req.nextUrl.pathname === '/auth/login') {
+    return NextResponse.redirect(new URL('/', req.url));
   }
 
-  // If the session token is present, continue with the request
+  // If the session token is present or the user is on the login page, continue with the request
   return NextResponse.next();
 }
 
 // Specify the paths where the middleware should be applied
 export const config = {
-  matcher: ['/messages/:path*','/organizations/:path*','/auth/:path*'], // Apply middleware to all routes under /protected
+  matcher: ['/messages/:path*', '/organizations/:path*', '/auth/:path*'], // Apply middleware to all routes under /protected
 };
