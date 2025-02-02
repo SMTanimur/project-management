421ee91225d58b7a7de67a4dc33eada84674dc27
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2025-02-02 19:37:55 +0600

    fix: Add missing semicolon in auth service type definition

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -12,7 +12,7 @@ export const auth = {
   ): Promise<{
     message: string;
     user: IUser;
-    expires: number
+    expires: number;
     token: string;
   }> => {
     return await api.post(API_PATHS.REGISTER, payload);
